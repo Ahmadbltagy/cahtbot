0e98f8cd74eb3c5f30b98dbc7135e5d3459f872f
Ahmad El Beltagy <elbeltagyahmad@gmail.com>
2020-09-16 23:26:59 +0200

    finish autoCorrect fun

--- chatbot.cpp
+++ chatbot.cpp
@@ -8,34 +8,44 @@ map<string, string> m;
 map<string, string>::iterator it;
 
 string autoCorrect(string q){
-    int num = 0;
-    string c = "";
-    for(it = m.begin(); it != m.end(); it++){
-        int count = 0;
-        for(int i = 0; i < (*it).first.size(); i++)
-            for(int j = 0; j < q.size(); j++)
-                if((*it).first[i] == q[j])
-                    count++;
-        if(count > num){
-            num = count;
-            c = (*it).first;
+    if(m[q] != ""){
+        return q;
+    }
+    else
+    {
+        int num = 0;
+        string c = "";
+        for(it = m.begin(); it != m.end(); it++){
+            int count = 0;
+            for(int i = 0; i < (*it).first.size(); i++)
+                for(int j = 0; j < q.size(); j++)
+                    if((*it).first[i] == q[j])
+                        count++;
+            if(count > num){
+                num = count;
+                c = (*it).first;
+            }
         }
-    }    
     return c;
+    }    
+  
 }
 
 
 
 void answer(string q){
     //this order must run on windows 10 that have bingweather app
-    if(m[q] == "I'll open Weather...")
+    if(m[autoCorrect(q)] == "I'll open Weather...")
         if( !( system("start bingweather:") ) )
-            cout<< m[q] <<endl;
+            cout<< m[autoCorrect(q)] <<endl;
         else
-            cout<<"You should have  windows os and download bing weather app\n";
-    else if(m[q]!= "")
-        cout<< m[q] <<endl;
-    else if(m[autoCorrect(q)] != "")
+            cout<<"You should have  windows os on your PC and install bing weather app\n";
+    else if(m[autoCorrect(q)] == "I'll open Excel...")
+        if( !( system("start excel") ) )
+            cout<< m[autoCorrect(q)] <<endl;
+        else
+            cout<<"You should have  windows os on your PC and install Excel\n";
+    else if(m[autoCorrect(q)]!= "")
         cout<< m[autoCorrect(q)] <<endl;
     else
         cout<< "Sorry, I can't help with that.\n";
