fe7b3f12970bdfaaa2c2e3ff6aa25d705ca0d753
Ahmad El Beltagy <elbeltagyahmad@gmail.com>
2020-09-16 23:08:36 +0200

    finish autoCorrect fun

--- chatbot.cpp
+++ chatbot.cpp
@@ -5,6 +5,26 @@
 using namespace std;
 
 map<string, string> m;
+map<string, string>::iterator it;
+
+string autoCorrect(string q){
+    int num = 0;
+    string c = "";
+    for(it = m.begin(); it != m.end(); it++){
+        int count = 0;
+        for(int i = 0; i < (*it).first.size(); i++)
+            for(int j = 0; j < q.size(); j++)
+                if((*it).first[i] == q[j])
+                    count++;
+        if(count > num){
+            num = count;
+            c = (*it).first;
+        }
+    }    
+    return c;
+}
+
+
 
 void answer(string q){
     //this order must run on windows 10 that have bingweather app
@@ -15,13 +35,12 @@ void answer(string q){
             cout<<"You should have  windows os and download bing weather app\n";
     else if(m[q]!= "")
         cout<< m[q] <<endl;
+    else if(m[autoCorrect(q)] != "")
+        cout<< m[autoCorrect(q)] <<endl;
     else
         cout<< "Sorry, I can't help with that.\n";
 }
 
-void autoCorrect(string q){
-    int count = 0;
-}
 
 int main(){
 
@@ -36,7 +55,7 @@ int main(){
         string q, a;
         while(getline(tchat, q) ){
             getline(tchat, a);
-            m[q] = a;
+            m.insert(make_pair(q, a) );
         }
     }
     
@@ -44,6 +63,8 @@ int main(){
     start:
     cout<<"\nAsk me\n\n";
     getline(cin, question);
+    if(question[0] >= 97)
+        question[0] -= 32;
     answer(question);
     goto start;
     return 0;
